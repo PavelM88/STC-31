9723af1c066a3fdc6e2561aee431a4fccde51f6b
Pavel Mamaev <mamai_88@mail.ru>
2020-10-06 17:19:29 +0300

    created class HelloWorld

--- src/main/java/homeWork2/work2_1/HelloWorld.java
+++ src/main/java/homeWork2/work2_1/HelloWorld.java
@@ -1,5 +1,19 @@
 package homeWork2.work2_1;
 
-public class HelloWorld {
+import java.util.Arrays;
 
+public class HelloWorld {
+    static String word;
+    static String[] word2 = new String[1];
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(word.length());
+
+        word2[1] = Arrays.toString(new String[]{word});
+        System.out.println(Arrays.toString(word2));
+
+        if (word == null) throw new Exception("Переменная равна null");
+        System.out.println(word.length());
+
+    }
 }
